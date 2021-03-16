cce6e357ad95802e060e38951675e9f3110890f2
Landon Reasoner <reasoner.landon90@gmail.com>
2021-03-16 21:25:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,18 @@ class Student
     DB[:conn].execute("DROP TABLE IF EXISTS students")
   end 
   
+  def save 
+    if self.id
+      self.update
+    else 
+      sql = <<-SQL
+      INSERT INTO students(name, grade) VALUES (?,?)
+      SQL
+      
+      DB[:conn].execute("SELECT last_insert_rowid()
+      FROM students")[0][0]
+    end 
+  
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
