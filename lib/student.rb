a24edc024070806230096e8efc029dcd4adbf025
Landon Reasoner <reasoner.landon90@gmail.com>
2021-03-16 21:38:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,15 @@ class Student
     grade = row[2]
     self.new(id, name, grade)
   end 
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first 
+  end 
+      
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
